86810a1d56b50bb52c2f088f848d088eb4bd131d
moritaka <moritaka325@163.com>
2022-06-17 10:19:22 +0800

    feat: dev

--- src/classify.ts
+++ src/classify.ts
@@ -1,24 +1,21 @@
-import XLSX from "xlsx";
 import path from "path";
 import { listDocFiles } from "./utils";
 import fs from "fs";
+import { EmployeeMap } from "./employee";
 
-const referenceFile = "ref.xlsx";
 
-const DUPLICATE_DEPART_NAME = "Duplicate";
 const PWD = process.cwd();
+// const PWD = './test';
 
 main();
 
 async function main() {
 
-  const map = createMap()
-
+  const map = new EmployeeMap(PWD);
   const files = await listDocFiles(PWD);
-
   const classifyPromise = files.map(async file => {
     const name = file.filename.split(".")[0].trim();
-    const depart = map.get(name) || "Unknown"
+    const depart = map.getDepartment(name) || "Unknown"
     const departPath = path.join(PWD, depart);
     return moveToDestFolder(departPath, file.filepath, path.join(departPath, file.filename))
   })
@@ -32,31 +29,4 @@ async function moveToDestFolder(folder: string, src: string, dest: string) {
   }
 
   return fs.promises.copyFile(src, dest)
-}
-
-function createMap() {
-  const workbook = XLSX.readFile(referenceFile);
-  const sheets = workbook.Sheets[workbook.SheetNames[0]];
-
-  const map = new Map<string, string>();
-
-  for (let i = 2; ; i++) {
-    const name = sheets[`A${i}`];
-    const depart = sheets[`B${i}`];
-
-    if(!name && !depart) {
-      break;
-    }
-    if (name && depart) {
-      const name_v = name.v;
-      const depart_v = depart.v;
-      if (map.has(name_v)) {
-        map.set(name_v, DUPLICATE_DEPART_NAME);
-      } else {
-        map.set(name_v, depart_v);
-      }
-    }
-  }
-
-  return map;
 }
---
+++ src/employee.ts
@@ -0,0 +1,51 @@
+
+import XLSX from "xlsx"
+import path from "path";
+const referenceFile = "ref.xlsx";
+const DUPLICATE_DEPART_NAME = "Duplicate";
+
+export class EmployeeMap {
+
+  private readonly map;
+
+  constructor(root: string) {
+    const file = path.join(root, referenceFile);
+    this.map = createMap(file)
+  }
+
+  getDepartment(filename: string): string | null {
+    for(let [name, depart] of this.map.entries()) {
+      if(filename.includes(name)) {
+        return depart
+      }
+    }
+    return null;
+  }
+}
+
+function createMap(file: string) {
+  const workbook = XLSX.readFile(file);
+  const sheets = workbook.Sheets[workbook.SheetNames[0]];
+
+  const map = new Map<string, string>();
+
+  for (let i = 2; ; i++) {
+    const name = sheets[`A${i}`];
+    const depart = sheets[`B${i}`];
+
+    if(!name && !depart) {
+      break;
+    }
+    if (name && depart) {
+      const name_v = name.v;
+      const depart_v = depart.v;
+      if (map.has(name_v)) {
+        map.set(name_v, DUPLICATE_DEPART_NAME);
+      } else {
+        map.set(name_v, depart_v);
+      }
+    }
+  }
+
+  return map;
+}
